2fb1d0f115b9a7e146989cf72694fe3f7a1ab2e1
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2025-04-23 23:52:47 +0500

    Add Vercel rewrite config for SPA routing

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: './',
+  // yoki agar kerak bo‘lsa:
+  // base: '/',
+  build: {
+    outDir: 'dist'
+  }
+});
+
+
+
